c9e061e8be2140493a8411accb479ba1eeb0e49a
Robin Deits <robin.deits@gmail.com>
2016-03-22 15:18:22 -0400

    switch to lcm pod

--- deps/build.jl
+++ deps/build.jl
@@ -23,28 +23,30 @@ provides(AptGet, Dict("libglib2.0-dev" => gobject,
 
 provides(Sources, Dict(
     URI("https://github.com/RobotLocomotion/libbot/archive/cc8d228b50847c4c55e6963b8ee95c237287547f.zip") => lcmgl_client,
-    URI("https://github.com/lcm-proj/lcm/releases/download/v1.3.0/lcm-1.3.0.zip") => lcm
-    ))
-
-provides(BuildProcess,
-    Dict(
-    Autotools(libtarget="src/liblcm.la") => lcm
+    URI("https://github.com/RobotLocomotion/lcm-pod/archive/8dba206f3403f01048b73ce10b6a17da58280fbe.zip") => lcm
     ))
 
 prefix = joinpath(BinDeps.depsdir(lcmgl_client), "..")
-lcmgl_args = ["BUILD_PREFIX=$(prefix)"]
+pods_args = ["BUILD_PREFIX=$(prefix)"]
 @osx_only begin
     include_path = joinpath(Homebrew.prefix(), "include")
     library_path = joinpath(Homebrew.prefix(), "lib")
-    lcmgl_args = vcat(lcmgl_args, ["INCLUDE_PATH=\$INCLUDE_PATH:$(include_path)", "LIBRARY_PATH=\$LIBRARY_PATH:$(library_path)"])
+    pods_args = vcat(pods_args, ["INCLUDE_PATH=\$INCLUDE_PATH:$(include_path)", "LIBRARY_PATH=\$LIBRARY_PATH:$(library_path)"])
 end
 
+provides(SimpleBuild,
+    (@build_steps begin
+        GetSources(lcm)
+        @build_steps begin
+            MakeTargets(joinpath(BinDeps.depsdir(lcm)), pods_args)
+        end
+    end), lcm)
 
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(lcmgl_client)
         @build_steps begin
-            MakeTargets(joinpath(BinDeps.depsdir(lcmgl_client)), lcmgl_args)
+            MakeTargets(joinpath(BinDeps.depsdir(lcmgl_client)), pods_args)
         end
     end), lcmgl_client)
 
